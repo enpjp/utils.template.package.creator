28b2a1e8d993034ce2c923df130506b88dd1f6d3
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2019-10-11 11:25:42 +0100

    Updates to documentation

---
+++ R/utils.template.package.creator.R
@@ -0,0 +1,20 @@
+#'utils.template.package.creator: A package for creating template packages.
+#'
+#'This package provides two helper functions to create packages incorporating
+#'markdown templates.
+#'
+#'\code{\link{make.template.package}}: is run from the R console inside R Studio
+#'and creates the package framework.
+#'
+#'\code{\link{post.package}}: is run from inside R Studio after the package has
+#'been loaded.
+#'
+#'@section External Requirements: As a minimum, you will need R, RStudio, Pandoc
+#'  and Git correctly installed on your system. Extensive operating dependent
+#'  instructions are avaialble from \url{http://cran.r-project.org}. Please be
+#'  careful of following unofficial instructions as some are just wrong.
+#'
+#'
+#'@docType package
+#'@name utils.template.package.creator
+NULL
